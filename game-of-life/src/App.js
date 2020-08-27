222525cfdc5b7c3b1fcc79cbc3467ebfdae81d63
eaczechova <ewa.czech@hotmail.com>
2020-08-27 13:35:40 +0200

    add working draft

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef} from 'react';
+import { ChromePicker } from 'react-color';
+import Grid from './Grid.js';
 import './App.css';
 import {
   GameTitle,
@@ -6,88 +8,209 @@ import {
   Rules,
   About,
   SectionTitle,
-  GameWrapper,
+  List,
+  GridWrapper,
   GameContainer,
+  ListElement,
   PresetsContainer,
   ButtonsContainer,
-  Game,
   Button,
-  Preset
+  ColorPickerWrapper,
+  Form
 } from './styles';
 
 function App() {
-  
+  const [tempSpeed, setTempSpeed] = useState(10);
+  const [speed, setSpeed] = useState(10);
+  const [rows, setRows] = useState(20);
+  const [cols, setCols] = useState(20);
+  let [generation, setGeneration] = useState(0);
+  const [color, setColor] = useState('#000');
+  const [ownLayout, setOwnLayout] = useState(false);
+
   const [grid, setGrid] = useState(() => {
-    const grid = new Array(20).fill(new Array(20).fill(0)).map(col => col.map(row => row = Math.floor(Math.random() * 2)));
+    // const grid = new Array(20).fill(new Array(20).fill(0)).map(col => col.map(row => row = Math.floor(Math.random() * 2)));
+    const grid = new Array(rows).fill(new Array(cols).fill(0)).map(col => col.map(row => row = 0));
     return grid
   });
 
-  const changeCell = (rows, col) =>{
-    const newGrid = [...grid];
-    newGrid[rows][col] = grid[rows][col] ? 0 : 1;
-    setGrid(newGrid);
+  const intervalRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  // Fills grid cells with random values of 0 or 1
+  // Run the function on component mount
+
+  const fillGrid = () => {
+    let filledGrid = [...grid];
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        filledGrid[i][j] = Math.floor(Math.random() * 2);
+      }
+    }
+
+    // for (let i = 0; i < rows; i+=2) {
+    //   for (let j = 0; j < cols; j++) {
+    //     filledGrid[i][j] = 1;
+    //   }
+    // }
+    setGrid(filledGrid);
   }
 
-  const clearGrid = () => {
-    const emptyGrid = new Array(20).fill(new Array(20).fill(0)).map(col => col.map(row => row = 0));
-    setGrid(emptyGrid);
+  const handleChangeComplete = (color) => {
+    setColor( color.hex );
+  };
+
+  const preset1 = () => {
+    let filledGrid = [...grid];
+    for (let i = 0; i < rows; i += 2) {
+      for (let j = 0; j < cols; j++) {
+        filledGrid[i][j] = 1;
+      }
+    }
+    setGrid(filledGrid);
   }
+
+  const preset2 = () => {
+    let filledGrid = [...grid];
+    for (let i = 0; i < rows; i ++) {
+      for (let j = 0; j < cols; j++) {
+        filledGrid[i][j % 2] = Math.floor(Math.random() * 2);
+      }
+    }
+    setGrid(filledGrid);
+  }
+
+  // Change state of selected grid cell; 
+
+  const changeSelectedCell = (rows, col) =>{
+    const newGrid = [...grid];
+    if (!isPlaying) {
+      newGrid[rows][col] = grid[rows][col] ? 0 : 1;
+      setGrid(newGrid);
+      setOwnLayout(true);
+    }
+  };
+  
+  const stopGame = () => {
+    const emptyGrid = new Array(rows).fill(new Array(cols).fill(0)).map(col => col.map(row => row = 0));
+    setIsPlaying(false);
+    setGrid(emptyGrid);
+    setGeneration(0);
+    setSpeed(1000);
+    setOwnLayout(false);
+  };
+
+
+  const toggle = () => {
+    setIsPlaying(!isPlaying);
+  }
+
+  //\\  row, col //\\
+  // 1. row -1, col -1
+  // 2. row -1, col
+  // 3. row - 1, col +1
+  // 4. row, col -1,
+  // 0. row, col
+  // 5. row, col +1,
+  // 6. row +1, col -1
+  // 7.  row +1, col
+  // 8.  row +1, col +1
+
+  const gridUpdate = () => {
+    const newGrid = [...grid];
+    
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        const cell = grid[row][col]; // value of cell 0 or 1
+        let neighbourCounter = 0;
+          // Iterating over 8 neighbouring cells
+          for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+              // To avoid couting cell itself
+              if (!(i === 0 && j === 0)) {
+                const cellRow = row + i;
+                const cellCol = col + j;
+                // To stay within the grid
+                if (cellRow > 0 && cellRow < grid.length && cellCol > 0 && cellCol < grid.length) {
+                  neighbourCounter += grid[cellRow][cellCol];
+                }
+              }
+            }
+          }
+     
+          if (cell === 0 && neighbourCounter === 3) {
+            newGrid[row][col] = 1;
+          } else if ((cell === 1 && neighbourCounter > 3) || (cell === 1 && neighbourCounter < 2)) {
+            newGrid[row][col] = 0;
+          }
+      }
+    }
+    setGrid(newGrid);
+    setGeneration(generation++);
+  }
+
+  useEffect(() => {
+    if (isPlaying && generation === 0) {
+      if(ownLayout) {
+        intervalRef.current = setInterval(gridUpdate, speed);
+      } else {
+      fillGrid();
+        intervalRef.current = setInterval(gridUpdate, speed);
+      }
+    } 
+    else if (isPlaying && generation > 0) {
+      intervalRef.current = setInterval(gridUpdate, speed);
+    } else if (!isPlaying) {
+      clearInterval(intervalRef.current);
+    } 
+  }, [isPlaying, speed]);
+
+  const handleSpeedChange = (e) => {
+    setTempSpeed(e.target.value);
+  }
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setSpeed(tempSpeed);
+  }
+  console.log("tempSpeed", grid);
 
   return (
     <>
       <GameTitle>Conway's "Game of Life"</GameTitle>
       <MainWrapper>
-        <GameWrapper>
-          <GameContainer>
-            <SectionTitle>Generation: #</SectionTitle>
-            <Game>
-              {grid.map((rows, r) =>
-                rows.map((col, c) =>
-                  <div
-                    onClick={(e) => changeCell(r, c)}
-                    // {
-                    // const newGrid = [...grid];
-                    // if ([rows][col] == 0) {
-                    //   console.log("enter")
-                    //   newGrid[rows][col] = 1
-                    // }
-
-                    // }}
-                    style={{
-                      width: 25,
-                      height: 25,
-                      background: [rows][col] ? "white" : "black",
-                      border: "1px solid black"
-                    }}/>
-                  )
-              )}
-            </Game>
+        <GameContainer>
+          <GridWrapper>
+            <SectionTitle>Generation: #{generation}</SectionTitle>
+            <Grid grid={grid} changeSelectedCell={changeSelectedCell} color={color}/>
             <ButtonsContainer>
-              <Button>Play</Button>
-              <Button>Pause</Button>
-              <Button onClick={() => clearGrid()}>Stop</Button>
+              <Button onClick={toggle}>{isPlaying ? 'Pause' : 'Start'}</Button>
+              <Button onClick={stopGame}>Stop</Button>
             </ButtonsContainer>
-          </GameContainer>
+          </GridWrapper>
           <PresetsContainer>
-            <Preset><div>Box</div><button>Preset 1</button></Preset>
-            <Preset><div></div><button>Preset 2</button></Preset>
-            <Preset><div></div><button>Preset 3</button></Preset>
-            <Preset><div></div><button>Preset 4</button></Preset>
+            <SectionTitle>Presets</SectionTitle>
+            <ButtonsContainer>
+              <Button onClick={preset1}>Preset 1</Button>
+              <Button onClick={preset2}>Preset 2</Button>
+            </ButtonsContainer>
+            <SectionTitle>Settings</SectionTitle>
+            <Form onSubmit={onFormSubmit}>
+              <input value={tempSpeed} onChange={handleSpeedChange} /><button disabled={isPlaying ? "true" : ""}>Ok</button></Form>
+            <ColorPickerWrapper>
+              <ChromePicker color={color} onChangeComplete={handleChangeComplete} />
+            </ColorPickerWrapper>
           </PresetsContainer>
-        </GameWrapper>
-        <Rules>
-          <SectionTitle>Rules</SectionTitle>
-          <ul>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-            <li>Lorem</li>
-          </ul>
-        </Rules>
+          <Rules>
+            <SectionTitle>Rules</SectionTitle>
+            <List>
+              <ListElement>Any live cell with fewer than two live neighbors dies</ListElement>
+              <ListElement>Any live cell with two or three live neighbors lives on to the next generation.</ListElement>
+              <ListElement>Any live cell with more than three live neighbors dies, as if by overpopulation.</ListElement>
+              <ListElement>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</ListElement>
+            </List>
+          </Rules>
+        </GameContainer>
         <About> 
           <SectionTitle>About</SectionTitle>
           <div>
--- game-of-life/src/styles.js
+++ game-of-life/src/styles.js
@@ -4,29 +4,48 @@ export const GameTitle = styled.h1`
     text-align: center;
     font-size: 2.5rem;
     padding: 3rem;
+    width: 100%;
 `;
 
 export const MainWrapper = styled.main`
-    width: 95%;
-    margin: 2rem auto;
+    width: 100%;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    flex-direction: columns;
+    flex-direction: row;
     border-radius: 5px;
     background-color: white;
     box-shadow: 2.5px 5px 4px #DCDCDC;
+    @media (min-width: 900px) {
+       flex-direction: column;
+        width: 95%; 
+        margin: 0 auto;
+	}
+`;
+
+export const GameContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid green;
+    @media (min-width: 900px) {
+       justify-content: space-between;
+	}
 `;
 
 export const Rules = styled.section`
-    width: 30%;
-    font-size: 2rem;
+    width: 100%;
+    font-size: 1.6rem;
+    @media (min-width: 900px) {
+        width: 20%;
+	}
 `;
 
 export const About = styled.section`
-    width: 100%;
+    width: 80%;
     font-size: 1.6rem;
-    padding: 2rem;
+    padding: 1rem;
+    margin: 5rem auto;
 `;
 
 export const SectionTitle = styled.h2`
@@ -34,48 +53,82 @@ export const SectionTitle = styled.h2`
     font-size: 2rem;
 `;
 
-export const GameWrapper = styled.div`
-    border: 1px solid green;
-    width: 68%;
+export const GridWrapper = styled.div`
+    width: 100%;
     display: flex;
-`;
-
-export const GameContainer = styled.div`
-    border: 1px solid green;
-    width: 80%;
-    display: flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
+    border: 1px solid blue;
+    @media (min-width: 900px) {
+        flex-direction: column;
+        align-items: baseline;
+        width: 55%;
+	}
 `;
 
 export const ButtonsContainer = styled.div`
     display: flex;
-    justify-content: space-evenly;
+    justify-content: center;
     padding: 3rem;
+    flex-direction: column;
+    @media (min-width: 900px) {
+        flex-direction: row;
+   
 `;
 
 export const PresetsContainer = styled.div`
-    border: 1px solid green; 
-    width: 20%;
+    width: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+    @media (min-width: 900px) {
+        width: 20%
 `;
 
 export const Game = styled.div`
+    width: 500px;
+    height: 500px;
     display: grid;
-    grid-template-columns: repeat(20, 25px);
+    overflow-x: hidden;
+    grid-template-columns: repeat(20, 2.5rem);
     margin: 0.5rem auto;
+`;
+
+export const Cell = styled.div`
+    width: 2.5rem;
+    height: 2.5rem;
+    border: 1px solid #D1C6C6;
 `;
 
 export const Button = styled.button`
     border-radius: 5px;
     padding: 1.5rem;
-    width: 8rem;    
+    width: 9rem;    
     cursor: pointer;
     background-color: transparent;
+    margin-right: 2rem;
 `;
 
-export const Preset = styled.div`
-border: 1px solid red;`
+export const Preset = styled.div``;
+   
+export const ListElement = styled.li`
+    font-size: 1.5rem;
+    padding: 1rem;
+`;
+
+export const List = styled.ul`
+    padding: 0;
+    text-align: justify;;
+    width: 80%;
+    margin: 0 auto;
+   
+`;
+
+export const ColorPickerWrapper = styled.div`
+    margin: 3rem auto;
+`;
+
+export const Form = styled.form`
+    text-align: center;
+`;
+
 
